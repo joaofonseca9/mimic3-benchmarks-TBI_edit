fa13f0b26fd3bada24673c8d8352ebf4a821e6b1
joaofonseca9 <74836152+joaofonseca9@users.noreply.github.com>
2022-05-23 13:22:58 +0100

    adding transformers

--- mimic3models/in_hospital_mortality/main_transformer.py
+++ mimic3models/in_hospital_mortality/main_transformer.py
@@ -7,11 +7,8 @@ from keras.layers.wrappers import Bidirectional, TimeDistributed
 from mimic3models.keras_utils import LastTimestep
 from mimic3models.keras_utils import ExtendMask
 
-from tensorflow import keras
-from tensorflow.keras import layers
+from mimic3models.keras_models.transformer import transformer_encoder, build_transformer_model
 
-from __future__ import absolute_import
-from __future__ import print_function
 from imblearn.over_sampling import SMOTE
 
 # import keras_tuner
@@ -234,44 +231,6 @@ elif args.addnoise:
 else:
     print("=> No oversampling\n")
 
-# Build the model
-def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
-    # Normalization and Attention
-    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
-    x = layers.MultiHeadAttention(
-        key_dim=head_size, num_heads=num_heads, dropout=dropout
-    )(x, x)
-    x = layers.Dropout(dropout)(x)
-    res = x + inputs
-
-    # Feed Forward Part
-    x = layers.LayerNormalization(epsilon=1e-6)(res)
-    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
-    x = layers.Dropout(dropout)(x)
-    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
-    return x + res
-
-def build_transformer_model(
-    input_shape,
-    head_size,
-    num_heads,
-    ff_dim,
-    num_transformer_blocks,
-    mlp_units,
-    dropout=0,
-    mlp_dropout=0,
-):
-    inputs = keras.Input(shape=input_shape)
-    x = inputs
-    for _ in range(num_transformer_blocks):
-        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
-
-    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
-    for dim in mlp_units:
-        x = layers.Dense(dim, activation="relu")(x)
-        x = layers.Dropout(mlp_dropout)(x)
-    outputs = layers.Dense(n_classes, activation="softmax")(x)
-    return keras.Model(inputs, outputs)
 
 def create_model(input_dim=X.shape[2],batch_size=args.batch_size, batch_norm=args.batch_norm, learning_rate=args.lr, 
                 depth=args.depth, dim=args.dim, dropout=args.dropout,mlp_dropout=args.rec_dropout,
