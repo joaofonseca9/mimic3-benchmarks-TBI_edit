320808fa7e4b56aabf7f2705342a8ac464786107
joaofonseca9 <74836152+joaofonseca9@users.noreply.github.com>
2022-05-23 21:06:18 +0100

    debugging transformer

--- mimic3models/keras_models/transformer.py
+++ mimic3models/keras_models/transformer.py
@@ -13,7 +13,7 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
 
     # Feed Forward Part
     x = layers.LayerNormalization(epsilon=1e-6)(res)
-    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="tanh")(x)
     x = layers.Dropout(dropout)(x)
     x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
     return x + res
@@ -35,7 +35,7 @@ def build_transformer_model(
 
     x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
     for dim in mlp_units:
-        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dense(dim, activation="sigmoid")(x)
         x = layers.Dropout(mlp_dropout)(x)
-    outputs = layers.Dense(n_classes, activation="softmax")(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
     return keras.Model(inputs, outputs)
