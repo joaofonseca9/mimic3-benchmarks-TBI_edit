15780ddcec408532b3861d7accd822f6f90d5788
joaofonseca9 <74836152+joaofonseca9@users.noreply.github.com>
2022-05-31 09:25:01 +0100

    Update keras_utils.py

--- mimic3models/keras_utils.py
+++ mimic3models/keras_utils.py
@@ -113,7 +113,7 @@ class InHospitalMortalityMetrics(keras.callbacks.Callback):
             max_auc = np.max([x["auroc"] for x in self.val_history])
             best_epoch=np.argmax([x["auroc"] for x in self.val_history])+1
             cur_auc = self.val_history[-1]["auroc"]
-            if max_auc > 0.85 and cur_auc < 0.82:
+            if max_auc > 0.85 and cur_auc < 0.82 and epoch>10:
                 self.model.stop_training = True
                 print('Best Epoch ', best_epoch, ' with ROC-AUC ', max_auc)
 
